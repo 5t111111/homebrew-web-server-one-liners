5159f9c8e70e2f0d30c19a8bbb8fb92a00f1bffb
Hirofumi Wakasugi <baenej@gmail.com>
2015-09-11 12:55:13 +0900

    Fix SHA256

--- web-server-one-liners.rb
+++ web-server-one-liners.rb
@@ -1,7 +1,7 @@
 require "formula"
 
 PACKAGE_VERSION = "0.1.1"
-PACKAGE_SHA = "a6c736189dc306239ab8d614e830950daa861a3bf51a5b3291f5c8e7920d9a6b"
+PACKAGE_SHA = "e43bf7acbe712da1aca80886eebcc5bebeee49ef4c2d42d2016fd6626a3e903b"
 
 class WebServerOneLiners < Formula
   homepage "https://github.com/5t111111/web-server-one-liners"
